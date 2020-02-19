d8b4031c9205a2130a1aa0590f0ef6545bad67ed
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-02-19 15:59:41 -0600

    added models

--- app.js
+++ app.js
@@ -2,32 +2,45 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 //const bcrypt = require ('bcrypt')
 const session = require('express-session')
 const cors = require('cors')
 //const jwt = require ("jsonwebtoken")
 const authentication = require('./middleware/authentication')
-const pgp = require('pg-promise')()
-pgp.pg.defaults.ssl = true;
+//const pgp = require('pg-promise')()
+//pgp.pg.defaults.ssl = true;
 //pgp.pg.defaults.rejectUnauthorized = false;
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const DATABASE_URL = process.env.DATABASE_URL;
-const db = pgp(DATABASE_URL)
+//const db = pgp(DATABASE_URL)
 //const SALT_ROUNDS = 10
+const user = require('./models/user')
 const registerRouter = require('./routes/register')
 
 
 app.use(cors())
 app.use(express.json())
-//app.use('/register', registerRouter)
+
+//connect to the database
+mongoose.connect(DATABASE_URL, {useNewUrlParser: true}, (error) => {
+  if(error) {
+    console.log('Unable to connect to the database!')
+  } else {
+    console.log("Connected to the database!")
+  }
+})
+
+
+app.use('/register', registerRouter)
  
 app.get('/', (req, res) => {
   res.send("HELLO")
 })
 
-app.listen(PORT, () => {
+app.listen(3011, () => {
   console.log('Running on PORT ', PORT)
 })
 
--- routes/register.js
+++ routes/register.js
@@ -1,24 +1,63 @@
 const express = require("express")
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const mongoose =require('mongoose')
 const SALT_ROUNDS = 10
 const jwt = require ("jsonwebtoken")
 const JWT_SECRET = process.env.JWT_SECRET
+const User = require('../models/user')
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   let name = req.body.name
   let username = req.body.username
   let password = req.body.password
   let confirmPassword = req.body.confirmPassword
+  console.log(name)
   
   if (password === confirmPassword) {
-    db.oneOrNone('SELECT username FROM users WHERE username = $1', [username]).then((user) => {
+    
+    const user =  await User.findOne({
+      username: username
+    })
+    
+    
+    // db.oneOrNone('SELECT username FROM users WHERE username = $1', [username]).then((user) => {
+      /*
+      User.findOne({username: username}, (error, user) => {
+        console.log(username)
+        console.log(user)
+         
+        if(!user) {
+          // if user does not exist then do this 
+        }
+        
+        
+      }) */
+      
       if(!user){
         // username available, save to database
         bcrypt.hash(password, SALT_ROUNDS)
         .then(hash => {
-          db.one("INSERT INTO users (name, username, hash) VALUES ($1, $2, $3) RETURNING id;", [name, username, hash])
+          //db.one("INSERT INTO users (name, username, hash) VALUES ($1, $2, $3) RETURNING id;", [name, username, hash])
+          const user = new User({
+            name: name,
+            username: username,
+            password: hash
+          })
           .then(results => {
+            
+            // user.save((error,results) => {
+              
+            // })
+            
+            user.save((error, results) => {
+              if(error){
+                console.log(error)
+              }
+              else {
+                res.send(results)
+              }
+            })
             const token = jwt.sign({user: username, id: results.id}, JWT_SECRET)
             res.status(200).json({token: token, id: results.id})
           })
@@ -35,15 +74,15 @@ router.post('/', (req, res) => {
       else {
         res.status(500).json({"message": "Error creating user."})
       }
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(500).json({"message": "Error creating user."})
-    })
+    }
+    // .catch(error => {
+    //   console.log(error)
+    //   res.status(500).json({"message": "Error creating user."})
+    // })
     
-  } else {
-    res.status(500).json({"message": "password don't match"})
-  }
+  // } else {
+  //   res.status(500).json({"message": "password don't match"})
+  // }
   
 })
 
